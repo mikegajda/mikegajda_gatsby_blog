526c8d79d0248c9c0d09bda41ffdb0e8950cfff2
Mike Gajda <mgajda@bu.edu>
2018-08-26 15:19:27 -0400

    First commit

---
+++ gatsby-config.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+
+module.exports = {
+  siteMetadata: {
+    title: `A sample site using gatsby-source-wordpress`,
+    subtitle: `Data fetched from a site hosted on wordpress.com`,
+  },
+  plugins: [
+    // https://public-api.wordpress.com/wp/v2/sites/gatsbyjsexamplewordpress.wordpress.com/pages/
+    /*
+     * Gatsby's data processing layer begins with “source”
+     * plugins. Here the site sources its data from Wordpress.
+     */
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        /*
+        * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
+        * Example : 'gatsbyjswpexample.wordpress.com' or 'www.example-site.com'
+        */
+        baseUrl: `mikegajda.wordpress.com`,
+        // The protocol. This can be http or https.
+        protocol: `http`,
+        // Indicates whether the site is hosted on wordpress.com.
+        // If false, then the asumption is made that the site is self hosted.
+        // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
+        // If your site is hosted on wordpress.org, then set this to false.
+        hostingWPCOM: true,
+        auth: {
+          // If hostingWPCOM is true then you will need to communicate with wordpress.com API
+          // in order to do that you need to create an app (of type Web) at https://developer.wordpress.com/apps/
+          // then add your clientId, clientSecret, username, and password here
+          wpcom_app_clientSecret: process.env.wpcom_app_clientSecret,
+          wpcom_app_clientId: process.env.wpcom_app_clientId,
+          wpcom_user: process.env.wpcom_user,
+          wpcom_pass: process.env.wpcom_pass,
+        },
+        // If useACF is true, then the source plugin will try to import the Wordpress ACF Plugin contents.
+        // This feature is untested for sites hosted on Wordpress.com
+        useACF: true,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-glamor`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
+      },
+    },
+  ],
+}
